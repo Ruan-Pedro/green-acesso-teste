cda998904ed12a4a18065bf7c1275aa63c3f2819
Ruan Pedro Mendes <72527894+Ruan-Pedro@users.noreply.github.com>
2023-07-15 08:34:04 -0300

    Update pdfOptions.js

--- src/utils/pdfOptions.js
+++ src/utils/pdfOptions.js
@@ -1,3 +1,4 @@
+//opções da documentação da biblioteca node-pdf-creator
 module.exports = options = {
     format: "A4",
     orientation: "portrait",
@@ -10,7 +11,7 @@ module.exports = options = {
         height: "28mm",
         contents: {
             first: 'Cover page',
-            2: 'Second page', // Any page number is working. 1-based index
+            2: 'Second page',
             default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
             last: 'Last Page'
         }
